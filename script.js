27c0fb6192d5d7871b134ac65a56b93fdfb71eb2
Gianfranco De Vettori <Ako@Akos-Mac-2.local>
2024-01-06 13:37:36 -0700

    starting script.js code

---
+++ script.js
@@ -0,0 +1,8 @@
+const apiKey = 
+const apiUrl = api.meteomatics.com/validdatetime/parameters/locations/format?optionals
+
+const searchForm = document.getElementById('search-form');
+const cityInput = document.getElementById('city-input');
+const currentWeather = document.getElementById('current-weather');
+const forecast = document.getElementById('forecast');
+const searchHistory = document.getElementById('search-history');
